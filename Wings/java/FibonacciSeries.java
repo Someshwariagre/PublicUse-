6de88082a1eff79b632cc5476ba7df6165d90ee0
unknown <someshwari.xworkz@gmail.com>
2022-10-20 15:35:28 +0530

    somu

---
+++ Wings/java/FibonacciSeries.java
@@ -0,0 +1,20 @@
+class FibonacciSeries{
+
+public static void main (String Args[]){
+ int a=0, b=1;
+ System.out.println(a+" "+b);
+  int c;
+ for(int i=2; i<=30; i++){
+
+   c=a+b;
+System.out.println(c);
+   a=b;
+  b=c;
+}
+}
+}
+
+ 
+
+
+
