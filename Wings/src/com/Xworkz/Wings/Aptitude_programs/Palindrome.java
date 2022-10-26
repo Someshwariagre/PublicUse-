b24942d9da7b9e8e2d99c4584a5ebf50ca0b046c
unknown <someshwari.xworkz@gmail.com>
2022-10-26 21:29:41 +0530

    somu

---
+++ Wings/src/com/Xworkz/Wings/Aptitude_programs/Palindrome.java
@@ -0,0 +1,25 @@
+package com.Xworkz.Wings.Aptitude_programs;
+
+public class Palindrome {
+	  public static void main(String[] args){
+          int no=121;
+          int temp=no;
+          int rev = 0,rem;
+          while(no!=0)
+          break;
+        {
+                 rem=temp%10;
+                 rev=rev*10+rem;
+                 temp=temp/10;
+}
+    if(no==rev)
+     {  
+  System.out.println(no+" is palindrome number");
+}
+   else{
+    System.out.println(no+" is not palindrome number");
+}
+}
+
+
+}
