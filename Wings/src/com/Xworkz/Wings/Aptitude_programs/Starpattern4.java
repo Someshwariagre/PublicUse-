73ce70f6324413954855987cceb6a7fb7a6997e9
unknown <someshwari.xworkz@gmail.com>
2022-10-27 08:12:17 +0530

    somu

---
+++ Wings/src/com/Xworkz/Wings/Aptitude_programs/Starpattern4.java
@@ -0,0 +1,17 @@
+class Starpattern1{
+
+public static void main (String Args[]){
+
+for(int i=1; i<=4; i++){
+for(int j=3; j>=i; j--){
+System.out.print(" ");
+}
+for (int k=1; k<=i; k++){
+
+System.out.print("*");
+}
+
+System.out.println();
+}
+}
+}
