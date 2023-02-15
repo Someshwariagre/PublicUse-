1017b547d43714d92bba7a8f11aec09271bd3071
unknown <someshwari.xworkz@gmail.com>
2023-02-15 15:58:09 +0530

    general

---
+++ Wings/src/com/Xworkz/Wings/General.java
@@ -0,0 +1,16 @@
+
+class Course{
+
+public static void main(String Args[]){
+ learnFramework();
+ learnJava();
+}
+ void learnFramework(){
+      
+System.out.println("I learnt framework");
+}
+
+void learnJava(){
+System.out.println("I learnt Java");
+}
+}
